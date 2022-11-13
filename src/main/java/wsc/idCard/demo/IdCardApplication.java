b4b088792a87265497edc6f51b0c5ae5967a5f65
aulianar <arisky540@gmail.com>
2022-11-13 23:18:05 +0700

    membuat index.html

---
+++ src/main/java/wsc/idCard/demo/IdCardApplication.java
@@ -0,0 +1,13 @@
+package wsc.idCard.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wsc/idCard/demo/IdCardApplicationTests.java
@@ -0,0 +1,13 @@
+package wsc.idCard.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
